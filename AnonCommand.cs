9c5ae5298efb3da6b945d8a0b3044e3fa928f54b
unknown <fancygiving7@gmail.com>
2021-04-06 00:50:27 +0300

    Initial Release

---
+++ AnonCommand.cs
@@ -0,0 +1,22 @@
+﻿using Rocket.API;
+using SDG.Unturned;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RPMessages
+{
+    public class AnonCommand : IRocketCommand
+    {
+        public AllowedCaller AllowedCaller => AllowedCaller.Player;
+        public string Name => "anontwt";
+        public string Help => "Anonymous message.";
+        public string Syntax => "<>";
+        public List<string> Aliases => new List<string>();
+        public List<string> Permissions => new List<string>() { "perm.anontwt" };
+
+        public void Execute(IRocketPlayer caller, string[] command)
+        {
+            ChatManager.serverSendMessage($"Anonymous: {command[0]}", Color.white, default, default, EChatMode.GLOBAL);
+        }
+    }
+}
---
+++ TwtCommand.cs
@@ -0,0 +1,25 @@
+﻿using Rocket.API;
+using Rocket.Unturned.Player;
+using SDG.Unturned;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RPMessages
+{
+    public class TwtCommand
+    {
+        public AllowedCaller AllowedCaller => AllowedCaller.Player;
+        public string Name => "twt ";
+        public string Help => "Twitter message.";
+        public string Syntax => "<>";
+        public List<string> Aliases => new List<string>();
+        public List<string> Permissions => new List<string>() { "perm.twt" };
+
+        public void Execute(IRocketPlayer caller, string[] command)
+        {
+            UnturnedPlayer player = caller as UnturnedPlayer;
+            string img = player.SteamProfile.AvatarIcon.ToString();
+            ChatManager.serverSendMessage($"[Twitter] {player.CharacterName}: {command[0]}", Color.white, default, default, EChatMode.GLOBAL, img);
+        }
+    }
+}
